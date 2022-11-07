105097eaa21ce8363dbb780e57b23d8c17ddc4ee
ding_chengjie <ding_chengjie@hoperun.com>
2022-11-07 19:11:50 +0800

    modify codestyle issues
    Signed-off-by: ding_chengjie <ding_chengjie@hoperun.com>

--- common/src/main/ets/utils/LogUtil.ts
+++ common/src/main/ets/utils/LogUtil.ts
@@ -12,10 +12,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import HiLog from "@ohos.hilog"
+
+import HiLog from "@ohos.hilog";
 
 const DOMAIN = 0x0500;
-const TAG = "[Recorder]"
+const TAG = "[Recorder]";
 
 /**
  *  log package tool class
--- product/phone/src/main/ets/MainAbility/MainAbility.ts
+++ product/phone/src/main/ets/MainAbility/MainAbility.ts
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import Ability from '@ohos.application.Ability'
 
 export default class MainAbility extends Ability {
