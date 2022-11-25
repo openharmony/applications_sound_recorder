8989bd514e377e4cc8bf62a58fc3aa9c24e72aff
liu_gang <liu_gang1@hoperun.com>
2022-11-25 16:43:24 +0800

    add recorder ohosTest
    Signed-off-by: liu_gang <liu_gang1@hoperun.com>

--- product/phone/src/ohosTest/ets/Application/TestAbilityStage.ts
+++ product/phone/src/ohosTest/ets/Application/TestAbilityStage.ts
@@ -17,5 +17,6 @@ import AbilityStage from "@ohos.application.AbilityStage"
 export default class TestAbilityStage extends AbilityStage {
     onCreate() {
         console.log("[Demo] TestAbilityStage onCreate")
+        globalThis.abilityContext = this.context
     }
 }
